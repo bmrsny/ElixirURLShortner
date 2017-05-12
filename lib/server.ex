c9076e0e81c2f93bf2635ee6037be8f2b096b049
Brandon Mrsly <bmrsny@gmail.com>
2017-05-12 15:13:25 -0600

    fixup! Initial setup for GenServer on URL Shortner

--- lib/server.ex
+++ lib/server.ex
@@ -3,6 +3,6 @@ defmodule Shortner.Server do
 
   @doc "initializes the genserver, which is looping infinitely, with an empty map."
   def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, %{}, [])
+    GenServer.start_link(__MODULE__, %{}, opts)
   end
 end
